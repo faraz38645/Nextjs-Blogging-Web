99425d3be7ef08f87f2c198acae9b60df911a04a
Faraz <95055562+faraz38645@users.noreply.github.com>
2023-08-19 21:38:11 +0500

    Delete next-tailwind-ecommerce-main directory

--- next-tailwind-ecommerce-main/src/pages/api/products.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import * as products from '../../../public/static/data/products.json';
-
-export default function handler(req, res) {
-  res.status(200).json(products)
-}
